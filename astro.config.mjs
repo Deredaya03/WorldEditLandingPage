4d5ecb41d37bf28803f0cac57237872ab2d8e4c6
Deredaya <138322582+Deredaya03@users.noreply.github.com>
2025-07-12 02:11:43 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://Worledit-be.deredaya.com/",
+  site: "https://Worledit-be.deredaya.com",
   vite: {
     plugins: [tailwindcss()],
   },
